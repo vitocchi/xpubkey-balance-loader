ea3fe01592bb6e266d200b2b459882cc3183ab07
Kosuke Ito <ito-kosuke0502@g.ecc.u-tokyo.ac.jp>
2019-04-10 14:55:09 +0900

    expressを導入

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 console.log('hello world');
+
+var express = require('express');
+
+var app = express();
+
+var server = app.listen(3000, function() {
+    console.log("Node.js is listening to PORT:" + server.address().port)
+})
+
+app.get('/', function(req,res,next) {
+    res.json('hello world')
+})
